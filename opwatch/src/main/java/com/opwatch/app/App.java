60df1c7a37b3c5b7787788ff7615ba9eeea2337b
Emgv <90226106+emgv0501@users.noreply.github.com>
2023-02-26 10:25:44 -0300

    First commit

---
+++ opwatch/src/main/java/com/opwatch/app/App.java
@@ -0,0 +1,13 @@
+package com.opwatch.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
